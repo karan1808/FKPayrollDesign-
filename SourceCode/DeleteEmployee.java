6b555b166fe7a5756a2b49ea9d9c5ebb06de666e
karan1808 <karanlamba1808@gmail.com>
2020-05-10 23:44:41 +0530

    made the second feature to delete a existing employee from the database

---
+++ SourceCode/DeleteEmployee.java
@@ -0,0 +1,49 @@
+package javafiles;
+
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+public class deleteEmployee{
+
+	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException{
+
+
+		System.out.println("Enter the id of employee which you want to delete");
+
+		Scanner in = new Scanner(System.in); 
+		int id = in.nextInt();
+
+		JSONParser parser = new JSONParser();
+
+		JSONArray arr = (JSONArray)parser.parse(new FileReader("Data.json"));
+		
+		int index = 0;
+
+		for (Object obj : arr){
+
+			
+		JSONObject employee = (JSONObject) obj;
+		Long employeeID =  (Long)(employee.get("employeeID"));
+		int temp = (int)(id);
+		if(employeeID == id){
+			break;
+		}
+		index++;
+		}
+		arr.remove(index);
+
+		FileWriter fileWriter = new FileWriter("employeesData.json");  
+		
+		fileWriter.write(arr.toJSONString());  
+        
+        fileWriter.flush();  
+        
+        fileWriter.close();  
+	}	
+}
