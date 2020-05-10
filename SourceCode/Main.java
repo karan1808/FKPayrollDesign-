4c4f99c398ef562dc9e083c0dab28e0374dc193f
karan1808 <karanlamba1808@gmail.com>
2020-05-10 18:38:38 +0530

    Created Main Page for the Portal Listing All the operations and taking input

---
+++ SourceCode/Main.java
@@ -0,0 +1,26 @@
+package javafiles;
+
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	public static void main(String [] args){
+
+	Scanner in = new Scanner(System.in);
+
+	System.out.println("Welcome to Flipkart Payroll Portal");
+	System.out.println("Select The Operation Which You Want To Perform");
+	System.out.println("1.Add a new employee");
+	System.out.println("2.Delete an employee");
+	System.out.println("3.Post a time card");
+	System.out.println("4.Post a sales receipt");
+	System.out.println("5.Post a union membership,service charge");
+	System.out.println("6.Change employee details");
+	System.out.println("7.Run the payroll for today");
+	
+	int operationInput;
+    operationInput = in.nextInt();
+
+
+	}
+}
